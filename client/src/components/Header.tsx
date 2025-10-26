71248d6ae9a35548c1d10770807a37724c768898
Alfredo Allan <allan-mecanico@outlook.com>
2025-10-26 19:45:56 -0300

    ajustando endereçamento de imagens

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -22,7 +22,7 @@ export function Header() {
         <div className="flex h-16 items-center justify-between gap-4">
           <div className="flex items-center gap-2">
             <img
-              src="../../image/instagran.png"
+              src="/image/instagran.png"
               alt="Logo Controle de Perecíveis"
               className="w-[45px] h-[45px] object-contain"
             />
